73ff801c29ed6d1964effe47f08dd85d285abb83
Saruar Chowdhury <56188614+Saruarchy@users.noreply.github.com>
2019-10-05 15:42:23 +0600

    Add files via upload

---
+++ Add Them Up.cpp
@@ -0,0 +1,15 @@
+/*Bismillahhir Rahmanir Rahim*/
+/*Saruar Chowdhury*/
+/*Metropolitan University*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d\n",c);
+
+    return 0;
+}
