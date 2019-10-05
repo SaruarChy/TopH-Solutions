06f01659ecb4dcb5c7320c0e71d8e385d0791be8
Saruar Chowdhury <56188614+Saruarchy@users.noreply.github.com>
2019-10-05 15:56:29 +0600

    Add files via upload

---
+++ Missing Number.cpp
@@ -0,0 +1,17 @@
+/*Bismillahhir Rahmanir Rahim*/
+/*Saruar Chowdhury*/
+/*Metropolitan University*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d",&a);
+    scanf("%d%d%d",&b,&c,&d);
+    e=a-(b+c+d);
+
+    printf("%d\n",e);
+
+    return 0;
+}
