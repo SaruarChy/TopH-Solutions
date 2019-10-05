f17df1b49cef634330ab6b0c0d38c0367f9029d7
Saruar Chowdhury <56188614+Saruarchy97@users.noreply.github.com>
2019-10-05 21:41:51 +0600

    Add files via upload

---
+++ Reverse Man.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a;
+    getline(cin,a);
+    string b=a;
+    reverse(b.begin(),b.end());
+    cout<<b<<endl;
+
+    return 0;
+}
