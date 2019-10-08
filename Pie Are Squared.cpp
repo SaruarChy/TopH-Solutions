08f864cd68651035d2068cffabc5fae7082bdb3b
Saruar Chowdhury <56188614+Saruarchy97@users.noreply.github.com>
2019-10-09 01:23:23 +0600

    Add files via upload

---
+++ Pie Are Squared.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI = acos(-1.0);
+int main(void)
+{
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(10) << PI*r*r << endl;
+
+    return 0;
+}
