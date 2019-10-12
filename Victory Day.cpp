a9f9644a22d9d745174084ad7b0437be720858c7
Saruar Chowdhury <56188614+Saruarchy97@users.noreply.github.com>
2019-10-12 15:48:49 +0600

    Add files via upload

---
+++ Victory Day.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        switch(n)
+        {
+            case 1:
+                cout<<"Bir Sreshtho Captain Mohiuddin Jahangir: Amra Tomay Vulbo Na."<<endl;
+                break;
+            case 2:
+                cout<<"Bir Sreshtho Sepahi Hamidur Rahman: Amra Tomay Vulbo Na."<<endl;
+                break;
+            case 3:
+                cout<<"Bir Sreshtho Sepahi Muhammad Mustafa: Amra Tomay Vulbo Na."<<endl;
+                break;
+            case 4:
+                cout<<"Bir Sreshtho Engine Room Artificer Mohammad Ruhul Amin: Amra Tomay Vulbo Na."<<endl;
+                break;
+            case 5:
+                cout<<"Bir Sreshtho Flight Lieutenant Matiur Rahman: Amra Tomay Vulbo Na."<<endl;
+                break;
+            case 6:
+                cout<<"Bir Sreshtho Lance Naik Munshi Abdur Rouf: Amra Tomay Vulbo Na."<<endl;
+                break;
+            case 7:
+                cout<<"Bir Sreshtho Lance Naik Noor Mohammad Sheikh: Amra Tomay Vulbo Na."<<endl;
+                break;
+        }
+    }
+}
