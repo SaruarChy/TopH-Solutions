76746de098ee04790d858a4bf6378ea0c88da826
Saruar Chowdhury <56188614+Saruarchy97@users.noreply.github.com>
2019-10-12 14:48:10 +0600

    Add files via upload

---
+++ Byang Loves Byangette.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int len;
+    getline(cin,s);
+    len=s.length();
+    if(len==20)
+        cout<<"Byang"<<endl;
+    else
+        cout<<"Byangette"<<endl;
+
+    return 0;
+}
