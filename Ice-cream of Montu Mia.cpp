f305415d9ec8f88d558bb9e6593371c73302939a
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-10-26 12:48:23 +0600

    Add files via upload

---
+++ Ice-cream of Montu Mia.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,b,c;
+    cin>>n;
+    cin>>a>>b>>c;
+    if(n-a>=10)
+        cout<<"Yes :-D"<<endl;
+    else if(n-b>=10)
+        cout<<"Yes :-D"<<endl;
+     else if(n-c>=10)
+        cout<<"Yes :-D"<<endl;
+    else
+        cout<<"No :-("<<endl;
+
+    return 0;
+}
