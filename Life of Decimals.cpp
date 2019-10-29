13861f03dc9b0d99f760a29b32a8b3bdbc1c0301
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-10-29 22:57:41 +0600

    Add files via upload

---
+++ Life of Decimals.cpp
@@ -0,0 +1,17 @@
+//{"3141592653589793238462643"};
+#include <bits/stdc++.h>
+#define Okay_bye return 0
+#define HI_Sexy int main()
+using namespace std;
+HI_Sexy
+{
+    int t,fuck;
+    char you[30]={"3141592653589793238462643"};
+    cin>>t;
+    while(t--)
+    {
+        scanf("%d",&fuck);
+        printf("%c\n",you[fuck]);
+    }
+    Okay_bye;
+}
