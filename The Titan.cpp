1f05b9002d59e921954623a42931460e3f4343d7
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-11-02 01:54:43 +0600

    Add files via upload

---
+++ The Titan.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#define     Copy_maris_nah      int main()
+#define     Okay_bye            return 0
+using namespace std;
+
+Copy_maris_nah
+{
+    long long n,i,a=0;
+    cin>>n;
+    for(i=1; i<=n; i++)
+        a+=i*i;
+    cout<<a<<endl;
+    Okay_bye;
+}
