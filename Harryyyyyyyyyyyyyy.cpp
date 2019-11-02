f321d68189c4d64fa3ee2227ef356b1ded99a209
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-11-03 00:57:53 +0600

    Add files via upload

---
+++ Harryyyyyyyyyyyyyy.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#define     Copy_maris_nah      int main()
+#define     Okay_bye            return 0
+using namespace std;
+
+Copy_maris_nah
+{
+    int t,a,i,j=1;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a;
+        cout<<"Case "<<j<<": "<<"Harr";
+        for(i=0; i<a; i++)
+        {
+            cout<<"y";
+        }
+        cout<<"! Khelbe Hogwarts, Jitbe Hogwarts!"<<endl;
+        j++;
+    }
+
+    Okay_bye;
+}
