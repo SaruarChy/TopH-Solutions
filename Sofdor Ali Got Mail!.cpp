64533468811ceaf552302db15630cec60b4d9551
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-11-03 02:16:39 +0600

    Add files via upload

---
+++ Sofdor Ali Got Mail!.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#define     Copy_maris_nah       int main()
+#define     Okay_bye             return 0;
+
+using namespace std;
+Copy_maris_nah
+{
+    cout<<"HI SOFDOR ALI"<<endl;
+    Okay_bye;
+}
