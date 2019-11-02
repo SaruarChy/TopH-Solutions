5c43dfee747ba3cbf58d27083d27573c8ca8e642
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-11-02 21:45:51 +0600

    Add files via upload

---
+++ Sofdor Ali's Magical Blackboard!.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#define     Copy_maris_nah      int main()
+#define     Okay_bye           return 0
+using namespace std;
+Copy_maris_nah
+{
+    long long int n,a,b,c;
+    cin>>n;
+    while(n--)
+    {
+        cin>>a>>b;
+        c=0;
+        c=a*b;
+        cout<<c<<endl;
+    }
+    Okay_bye;
+}
