b9fe76ceb9a33f9cff5a0b99f1670d558daa538b
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-11-03 01:28:57 +0600

    Add files via upload

---
+++ A Girl's Story.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#define     Copy_maris_nah       int main()
+#define     Okay_bye             return 0;
+
+using namespace std;
+Copy_maris_nah
+{
+    int x,y,b,c;
+    cin>>x>>y;
+    b=y-x;
+    c=b/2;
+    cout<<x+c<<endl;
+
+    Okay_bye;
+}
