4226c713ecc73dc6ae2c1c5b44b432c891c1eae0
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-10-18 02:09:11 +0600

    Add files via upload

---
+++ When Will Corona Go.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    cout<<"The date Corona will go."<<endl;
+
+    return 0;
+}
