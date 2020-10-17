7d362e91d5cc35c6cb3d6b92899e9c6f3bd368da
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-10-18 02:09:29 +0600

    Add files via upload

---
+++ Facebook Reacts.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    string s;
+    cin>>s;
+    if(s[0] == 'L' && s[1] == 'i' && s[2] == 'k' && s[3] == 'e'){
+        cout<<"(y)"<<endl;
+    }
+    else if(s[0] == 'L' && s[1] == 'o' && s[2] == 'v' && s[3] == 'e'){
+        cout<<"<3"<<endl;
+    }
+    else if(s[0] == 'C' && s[1] == 'a' && s[2] == 'r' && s[3] == 'e'){
+        cout<<"^_<3_^"<<endl;
+    }
+    else if(s[0] == 'H' && s[1] == 'a' && s[2] == 'h' && s[3] == 'a'){
+        cout<<":p"<<endl;
+    }
+    else if(s[0] == 'W' && s[1] == 'o' && s[2] == 'w'){
+        cout<<":O"<<endl;
+    }
+    else if(s[0] == 'S' && s[1] == 'a' && s[2] == 'd'){
+        cout<<":'("<<endl;
+    }
+    else{
+        cout<<">_<"<<endl;
+    }
+
+
+    return 0;
+}
+
